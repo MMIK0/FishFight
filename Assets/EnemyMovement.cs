dd2d10346c15024ae5b5b319d7905514d369a3b7
MMIK0 <35567342+MMIK0@users.noreply.github.com>
2022-06-11 13:21:37 +0300

    Something

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -6,12 +6,25 @@ public class EnemyMovement : MonoBehaviour
 {
     public float enemyMoveSpeed;
     public int enemyDamage;
+    public Vector3 target;
+    public enemyType typeOfEnemy;
+
+    public enum enemyType
+    {
+        red,
+        blue,
+        white
+    }
 
     void FixedUpdate()
     {
         if (Player.instance != null)
         {
-            transform.position = Vector2.MoveTowards(transform.position, Player.instance.transform.position, enemyMoveSpeed * Time.fixedDeltaTime);
+            transform.position = Player.instance.transform.position;
+            transform.rotation = Quaternion.Euler(Player.instance.transform.position.x, Player.instance.transform.position.y, Player.instance.transform.position.x);
+            target = Player.instance.transform.position;
+            transform.RotateAround(target, Vector3.back, 20 * Time.deltaTime);
         }
     }
+
 }
