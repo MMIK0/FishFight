ed0c2688cb4f449cf32ba5e924b241830b136625
Rastenbe <rasmus.criticalcharm@gmail.com>
2022-08-06 18:17:11 +0300

    changes

---
+++ Assets/EnemySquadron.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySquadron : MonoBehaviour
+{
+    private List<Transform> enemies;
+    public Vector3 squadCenter;
+
+    private void Update()
+    {
+        //Check for enemies to leave
+        //update all enemypoitions
+    }
+
+    public List<Transform> SortList()
+    {
+        return new List<Transform>();
+    }
+
+}
---
+++ Assets/SpawnList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SpawnList : ScriptableObject
+{
+    public List<TimeBasedSpawn> spawns;
+    public TimeBasedSpawn currentSpawnlist =new TimeBasedSpawn();
+    
+    [System.Serializable]
+    public class TimeBasedSpawn
+    {
+        public float spawnTime;
+        public List<GameObject> thingsToSpawn = new List<GameObject>();
+    }
+    public void SetCurrentSpawn(TimeBasedSpawn s)
+    {
+        currentSpawnlist = s;
+    }
+    public GameObject GetRandomSpawn()
+    {
+        return currentSpawnlist.thingsToSpawn[Random.Range(0, currentSpawnlist.thingsToSpawn.Count)];
+    }
+    public TimeBasedSpawn GetSpawnList(float time)
+    {
+        TimeBasedSpawn t=null;
+        foreach(TimeBasedSpawn i in spawns)
+        {
+            if (t == null)
+                t = i;
+            else if (time >= i.spawnTime && Mathf.Abs(i.spawnTime - time) < Mathf.Abs(t.spawnTime - time))
+                t = i;
+        }
+        return t;
+    }
+}
